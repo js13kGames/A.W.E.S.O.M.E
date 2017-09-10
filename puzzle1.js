1fb5b9a066c60c201daf6bd0bd55c155e0684885
Darren <30450554+dtvault@users.noreply.github.com>
2017-09-10 17:07:49 +0100

    Update puzzle1.js

--- puzzle1.js
+++ puzzle1.js
@@ -14,70 +14,70 @@ P1.Init = function() {
   for (i=0;i<=1;i++)
   {
     var getNumber = Math.floor(Math.random() * P1.Numbers.length);
-	var getLetter = Math.floor(Math.random() * P1.Letters.length);
+    var getLetter = Math.floor(Math.random() * P1.Letters.length);
 	
     var numCode = parseInt(P1.ShowChar(P1.Numbers[getNumber]));
     var letterCode = parseInt(P1.Letters[getLetter]);
-	
-	if (numCode > 0 && numCode < 9)
-	{
-	  var num1 = numCode - 1;
+
+    if (numCode > 0 && numCode < 9)
+    {
+      var num1 = numCode - 1;
       var num2 = numCode + 1;
 
-	  P1.problemNumber[i] = num1+''+num2;
+      P1.problemNumber[i] = num1+''+num2;
       P1.solution += numCode;
-	}
+    }
 	
-	// Special case
-	if (numCode == 0)
-	{
-	  var num1 = numCode + 1; // 1
-      var num2 = num1 + 2; // 3
-	  
-	  P1.problemNumber[i] = num1+''+num2;
-	  P1.solution += num2-1; // 2
-	}
+    // Special case
+    if (numCode == 0)
+    {
+      var num1 = numCode + 1;
+      var num2 = num1 + 2;
+ 
+      P1.problemNumber[i] = num1+''+num2;
+      P1.solution += num2 - 1;
+    }
 	
-	// Special case
-	if (numCode == 9)
-	{
-	  var num1 = numCode - 1; // 8
-      var num2 = num1 - 2; // 6
+    // Special case
+    if (numCode == 9)
+    {
+      var num1 = numCode - 1;
+      var num2 = num1 - 2;
 
       P1.problemNumber[i] = num2+''+num1;
-      P1.solution += num2+1;	
-	}
+      P1.solution += num2 + 1;	   
+    }
 	
-	// Get letters
-	if (letterCode > 65 && letterCode < 70)
-	{
-	  var num1 = letterCode - 1;
+    // Get letters
+    if (letterCode > 65 && letterCode < 70)
+    {
+      var num1 = letterCode - 1;
       var num2 = letterCode + 1;
 
-	  P1.problemLetter[i] = P1.ShowChar(num1)+''+P1.ShowChar(num2);
+      P1.problemLetter[i] = P1.ShowChar(num1)+''+P1.ShowChar(num2);
       P1.solution += P1.ShowChar(letterCode);
-	}
+    }
+  
+    // Special case
+    if (letterCode == 65)
+    {
+      var num1 = letterCode + 1;
+      var num2 = num1 + 2;
+      var num3 = num2-1;
+	   
+      P1.problemLetter[i] = P1.ShowChar(num1)+''+P1.ShowChar(num2);
+      P1.solution += P1.ShowChar(num3);
+    }
 	
-	// Special case
-	if (letterCode == 65)
-	{
-	  var num1 = letterCode + 1;
-	  var num2 = num1 + 2;
-	  var num3 = num2-1;
-	  
-	  P1.problemLetter[i] = P1.ShowChar(num1)+''+P1.ShowChar(num2);
-	  P1.solution += P1.ShowChar(num3);
-	}
-	
-	if (letterCode == 70)
-	{
-	  var num1 = letterCode - 1;
+    if (letterCode == 70)
+    {
+      var num1 = letterCode - 1;
       var num2 = num1 - 2;
       var num3 = num2+1;
 	  
       P1.problemLetter[i] = P1.ShowChar(num2)+''+P1.ShowChar(num1);
       P1.solution += P1.ShowChar(num3); 	  
-	}
+    }
   }
 }
 
@@ -97,12 +97,12 @@ P1.Render = function() {
 P1.Update = function() {
   if (T.GetKeyCode() == 13)
   {
-	KB.SetKeyCode(0);
+    KB.SetKeyCode(0);
+
+    P1.ShowMsg("HINT","Look between the numbers and letters, what can you see?");
+    P1.ShowMsg("I GIVE UP",P1.solution);
 	
-	P1.ShowMsg("HINT","Look between the numbers and letters, what can you see?");
-	P1.ShowMsg("I GIVE UP",P1.solution);
-	
-	KB.ClearBuffer();
+    KB.ClearBuffer();
   }
 }
 
